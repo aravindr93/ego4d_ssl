7de2c0207db63f2163d17d72780958c88445fafb
Aryan Jain <aryanjn2002@gmail.com>
2023-01-06 22:35:17 +0000

    launch scripts

--- dynamics_model/adaptation_launchers/dmc.sh
+++ dynamics_model/adaptation_launchers/dmc.sh
@@ -1,7 +1,8 @@
 # wandb login --host=https://fairwandb.org/ --relogin
 
 # Start with MoCo checkpoint trained on Ego4D (released model)
-PYTHONPATH=. python main_launcher.py environment.slurm=True \
+PYTHONPATH=. python main_launcher.py environment.slurm=False \
+    dynamics='inverse' \
     logging.wandb_project="inverse_dynamics_adapt_rep" logging.name="inverse_dynamics_dmc" \
     environment.ngpu=8 environment.world_size=1 \
     model.embedding='moco' \
